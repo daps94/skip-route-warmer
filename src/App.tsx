efffaafc086950c97bb611fd5dfd7f20f107925c
plubber <er.h.gorski@gmail.com>
2024-08-13 08:35:48 -0400

    wip: simulate transfer msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,104 @@
-import React, { useEffect } from "react";
-import { Balances } from "./types/balance";
+import React, { useState } from "react";
 import { Dec, DecUtils } from "@keplr-wallet/unit";
-import { isAddress } from "@ethersproject/address";
 import { sendMsgs } from "./util/sendMsgs";
-import { api } from "./util/api";
 import { simulateMsgs } from "./util/simulateMsgs";
-import { MsgSend } from "./proto-types-gen/src/cosmos/bank/v1beta1/tx";
 import "./styles/container.css";
 import "./styles/button.css";
 import "./styles/item.css";
-import { assets, chains, ibc,} from 'chain-registry';
+import { assets, chains } from 'chain-registry';
 import { chainRegistryChainToKeplr } from '@chain-registry/keplr';
 import { ChainInfo } from '@keplr-wallet/types';
+import { MsgTransfer } from "./proto-types-gen/src/ibc/applications/transfer/v1/tx";
+import { Coin } from "./proto-types-gen/src/cosmos/base/v1beta1/coin";
+import { Height } from "./proto-types-gen/src/ibc/core/client/v1/client";
 
 function App() {
-  const [address, setAddress] = React.useState<string>("");
-  // const [balance, setBalance] = React.useState<string>("");
-  const [denom, setDenom] = React.useState<string>("");
-  const [recipient, setRecipient] = React.useState<string>("");
-  const [originChain, setOriginChain] = React.useState<string>("");
-  const [amount, setAmount] = React.useState<string>("");
-  console.log('chains', chains)
+  const [address, setAddress] = useState("");
+  const [denom, setDenom] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [originChain, setOriginChain] = useState("");
+  const [amount, setAmount] = useState("");
+  const [channelId, setChannelId] = useState("");
+  const [chainInfo, setChainInfo] = useState<ChainInfo | null>(null);
 
-  // useEffect(() => {
-  //   init();
-  // }, []);
+  const onChainSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedChainId = e.target.value;
+    setOriginChain(selectedChainId);
 
-  // const init = async () => {
-  //   const keplr = window.keplr;
-  //   if (keplr) {
-  //     try {
-  //       await keplr.experimentalSuggestChain(OsmosisChainInfo);
-  //     } catch (e) {
-  //       if (e instanceof Error) {
-  //         console.log(e.message);
-  //       }
-  //     }
-  //   }
-  // };
-
-  const getKeyFromKeplr = async () => {
-    const key = await window.keplr?.getKey(OsmosisChainInfo.chainId);
-    if (key) {
-      setAddress(key.bech32Address);
+    const selectedChain = chains.find(chain => chain.chain_id === selectedChainId);
+    if (selectedChain) {
+      const config: ChainInfo = chainRegistryChainToKeplr(selectedChain, assets, {
+        getRestEndpoint: (chain) => chain.apis?.rest?.[0]?.address ?? "",
+        getRpcEndpoint: (chain) => chain.apis?.rpc?.[0]?.address ?? ""
+      });
+      console.log('config', config);
+      setChainInfo(config);
     }
   };
 
-  // const getBalance = async () => {
-  //   const key = await window.keplr?.getKey(OsmosisChainInfo.chainId);
+  const getKeyFromKeplr = async () => {
+    if (chainInfo) {
+      const key = await window.keplr?.getKey(chainInfo.chainId);
+      if (key) {
+        setAddress(key.bech32Address);
+      }
+    }
+  };
 
-  //   if (key) {
-  //     const uri = `${OsmosisChainInfo.rest}/cosmos/bank/v1beta1/balances/${key.bech32Address}?pagination.limit=1000`;
+  const sendIBCTransfer = async () => {
+    if (window.keplr && chainInfo) {
+      const key = await window.keplr?.getKey(chainInfo.chainId);
+  
+      
+      const timeoutHeight: Height = {
+        revisionNumber: "0",
+        revisionHeight: "0",
+      };
 
-  //     const data = await api<Balances>(uri);
-  //     const balance = data.balances.find(
-  //       (balance) => balance.denom === "uosmo"
-  //     );
-  //     const osmoDecimal = OsmosisChainInfo.currencies.find(
-  //       (currency) => currency.coinMinimalDenom === "uosmo"
-  //     )?.coinDecimals;
+      const timeoutTimestamp = (Math.floor(Date.now() / 1000) + 600) * (1000000000); // 10 minutes from now, in nanoseconds
 
-  //     if (balance) {
-  //       const amount = new Dec(balance.amount, osmoDecimal);
-  //       setBalance(`${amount.toString(osmoDecimal)} OSMO`);
-  //     } else {
-  //       setBalance(`0 OSMO`);
-  //     }
-  //   }
-  // };
+      const token: Coin = {
+        denom: denom,
+        amount: DecUtils.getTenExponentN(6)
+          .mul(new Dec(amount))
+          .truncate()
+          .toString(),
+      };
 
-  const onChainSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const msg: MsgTransfer = {
+        sourcePort: "transfer",
+        sourceChannel: channelId,
+        token: token,
+        sender: key.bech32Address,
+        receiver: recipient,
+        timeoutHeight: timeoutHeight,
+        timeoutTimestamp: timeoutTimestamp.toString(),
+        memo: "",
+      };
+      console.log('msg', msg)
 
-
-  const config: ChainInfo = chainRegistryChainToKeplr(chain, assets);
-
-// you can add options as well to choose endpoints 
-const config: ChainInfo = chainRegistryChainToKeplr(chain, assets, {
-    getRestEndpoint: (chain) => chain.apis?.rest[1]?.address
-    getRpcEndpoint: (chain) => chain.apis?.rpc[1]?.address
-});
-
-  const sendBalance = async () => {
-    if (window.keplr) {
-      const key = await window.keplr?.getKey(originChain);
-      const protoMsgs = {
-        typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-        value: MsgSend.encode({
-          fromAddress: key.bech32Address,
-          toAddress: recipient,
-          amount: [
-            {
-              denom,
-              amount: DecUtils.getTenExponentN(6)
-                .mul(new Dec(amount))
-                .truncate()
-                .toString(),
-            },
-          ],
-        }).finish(),
+      const protoMsg = {
+        typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+        value: MsgTransfer.encode(msg).finish(),
       };
 
       try {
         const gasUsed = await simulateMsgs(
-          OsmosisChainInfo,
+          chainInfo,
           key.bech32Address,
-          [protoMsgs],
-          [{ denom: "uosmo", amount: "236" }]
+          [protoMsg],
+          [{ denom: chainInfo.feeCurrencies[0].coinMinimalDenom, amount: "236" }]
         );
+        console.log('gasUsed', gasUsed)
 
         if (gasUsed) {
           await sendMsgs(
             window.keplr,
-            OsmosisChainInfo,
+            chainInfo,
             key.bech32Address,
-            [protoMsgs],
+            [protoMsg],
             {
-              amount: [{ denom: "uosmo", amount: "236" }],
+              amount: [{ denom: chainInfo.feeCurrencies[0].coinMinimalDenom, amount: "236" }],
               gas: Math.floor(gasUsed * 1.5).toString(),
             }
           );
@@ -131,92 +114,75 @@ const config: ChainInfo = chainRegistryChainToKeplr(chain, assets, {
   return (
     <div className="root-container">
       <div className="item-container">
-        {/* <div className="item">
-          <div className="item-title">Get OSMO Address</div>
+        <div className="item">
+          <div className="item-title">Warm IBC Route</div>
 
           <div className="item-content">
             <div>
-              <button className="keplr-button" onClick={getKeyFromKeplr}>
-                Get Address
-              </button>
-            </div>
-            <div>Address: {address}</div>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="item-title">Get OSMO Balance</div>
-
-          <div className="item-content">
-            <button className="keplr-button" onClick={getBalance}>
-              Get Balance
-            </button>
-
-            <div>Balance: {balance}</div>
-          </div>
-        </div> */}
-
-        <div className="item">
-          <div className="item-title">Warm Route</div>
-
-          <div className="item-content">
-            origin chain
-            <select>
-              {chains.map((chain) => (
-                <option key={chain.chain_id} onChange={onChainSelect} value={chain.chain_id}>
-                  {chain.chain_name}
-                </option>
-              ))}
-            </select>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              recipient
-              <input
-                type="text"
-                value={recipient}
-                onChange={(e) => setRecipient(e.target.value)}
-              />
+              Origin Chain
+              <select value={originChain} onChange={onChainSelect}>
+                <option value="">Select a chain</option>
+                {chains.map((chain) => (
+                  <option key={chain.chain_id} value={chain.chain_id}>
+                    {chain.chain_name}
+                  </option>
+                ))}
+              </select>
             </div>
 
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              amount
-              <input
-                type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-              />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              channel-id
-              <input
-                type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-              />
-            </div>
+            {chainInfo && (
+              <>
+                <div>
+                  <button className="keplr-button" onClick={getKeyFromKeplr}>
+                    Get Address
+                  </button>
+                  <div>Address: {address}</div>
+                </div>
 
-            <button className="keplr-button" onClick={sendBalance}>
-              Warm
-            </button>
+                <div>
+                  Recipient
+                  <input
+                    type="text"
+                    value={recipient}
+                    onChange={(e) => setRecipient(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  Amount
+                  <input
+                    type="text"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  Denom
+                  <input
+                    type="text"
+                    value={denom}
+                    onChange={(e) => setDenom(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  Channel ID
+                  <input
+                    type="text"
+                    value={channelId}
+                    onChange={(e) => setChannelId(e.target.value)}
+                  />
+                </div>
+
+                <button className="keplr-button" onClick={sendIBCTransfer}>
+                  Warm IBC Route
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
-
-     
     </div>
   );
 }
